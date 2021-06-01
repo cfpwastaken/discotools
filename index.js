e3d4289aee519fdb2520ea5c8aea922e301138ce
cfp <chaosflo44plus@gmail.com>
2021-06-01 19:55:58 +0200

    Stopped being stupid and put everything inside a class

--- index.js
+++ index.js
@@ -1,81 +1,74 @@
 const { APIMessage, MessageEmbed } = require("discord.js");
 
-var client;
-
-function discotools(client) {
-    this.client = client;
-    return this;
-}
-
-discotools.embed = (title, desc, color, footer) => {
-    let embed = new MessageEmbed()
-        .setTitle(title)
-        .setDescription(desc)
-        .setColor(color)
-        .setFooter(footer + " | ServerSystem");
-        return embed;
-};
-
-discotools.createAPIMessage = async function (interaction, content) {
-    const { data, files } = await APIMessage.create(
-        client.channels.resolve(interaction.channel_id),
-        content
-    ).resolveData().resolveFiles();
-
-    return { ...data, files };
-};
-
-discotools.reply = async function (interaction, response) {
-    let data = {
-        content: response
-    };
-
-    if(typeof response === 'object') {
-        // data = await this.createAPIMessage(interaction, response);       doesn't work
-        const { data: da, files } = await APIMessage.create(
-            client.channels.resolve(interaction.channel_id),
-            response
-        ).resolveData().resolveFiles();
-
-        data = { ...da, files };
+class DiscoTools {
+    constructor(_client) {
+        this.client = _client;
     }
-
-    client.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-            type: 4,
-            data
+    embed = (title, desc, color, footer) => {
+        let embed = new MessageEmbed()
+            .setTitle(title)
+            .setDescription(desc)
+            .setColor(color)
+            .setFooter(footer + " | ServerSystem");
+        return embed;
+    }
+    createAPIMessage = async function (interaction, content) {
+        const { data, files } = await APIMessage.create(
+            this.client.channels.resolve(interaction.channel_id),
+            content
+        ).resolveData().resolveFiles();
+    
+        return { ...data, files };
+    }
+    reply = async function (interaction, response) {
+        let data = {
+            content: response
+        };
+    
+        if(typeof response === 'object') {
+            // data = await this.createAPIMessage(interaction, response);       doesn't work
+            const { data: da, files } = await APIMessage.create(
+                this.client.channels.resolve(interaction.channel_id),
+                response
+            ).resolveData().resolveFiles();
+    
+            data = { ...da, files };
         }
-    });
-};
-
-discotools.newGuildCommand = async function (guildId, name, description, options) {
-    if(!options) options = [];
-    await client.api.applications(client.user.id).guilds(guildId).commands.post({
-        data: {
-            name: name,
-            description: description,
-            options
-        },
-    });
+    
+        this.client.api.interactions(interaction.id, interaction.token).callback.post({
+            data: {
+                type: 4,
+                data
+            }
+        });
+    }
+    newGuildCommand = async function (guildId, name, description, options) {
+        if(!options) options = [];
+        await this.client.api.applications(this.client.user.id).guilds(guildId).commands.post({
+            data: {
+                name: name,
+                description: description,
+                options
+            },
+        });
+    }
+    newGlobalCommand = async function (name, description, options) {
+        if(!options) options = [];
+        await this.client.api.applications(this.client.user.id).commands.post({
+            data: {
+                name: name,
+                description: description,
+                options
+            },
+        });
+    }
+    deleteGuildCommand = async function (guildId, commandId) {
+        await this.client.api.applications(this.client.user.id).guilds(guildId).commands(commandId).delete();
+    }
+    deleteGlobalCommand = async function (commandId) {
+        await this.client.api.applications(this.client.user.id).commands(commandId).delete();
+    }
 }
 
-discotools.newGlobalCommand = async function (name, description, options) {
-    if(!options) options = [];
-    await client.api.applications(client.getBot().user.id).commands.post({
-        data: {
-            name: name,
-            description: description,
-            options
-        },
-    });
-}
 
-discotools.deleteGuildCommand = async function (guildId, commandId) {
-    await client.api.applications(client.getBot().user.id).guilds(guildId).commands(commandId).delete();
-}
-
-discotools.deleteGlobalCommand = async function (commandId) {
-    await client.api.applications(client.getBot().user.id).commands(commandId).delete();
-}
-
-module.exports.discotools = discotools;
+module.exports.DiscoTools = DiscoTools;
