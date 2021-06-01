c838901d96e393fcbff265e2ad1a9c078806dbde
cfp <chaosflo44plus@gmail.com>
2021-06-01 19:33:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# DiscoTool
+
+## Installation
+
+Do `npm i discotools`
+
+## How to use
+
+Soon ¯\\_(ツ)\_/¯
---
+++ test/test.js
@@ -0,0 +1,4 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+const discotools = require("../index").discotools(client);
+console.log(discotools);
